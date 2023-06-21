eadc1e41b610d419dc34bb646cf37c7fafc33308
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-06-21 20:27:20 +0200

    Add basic js calculator functions

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hello");
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(a, operator, b) {
+    if (operator === "+") return add(a, b);
+    if (operator === "-") return sub(a, b);
+    if (operator === "*") return mul(a, b);
+    if (operator === "/") return div(a, b);
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
